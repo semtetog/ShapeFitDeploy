aefadda107c3a555fbff42a913b011f03994f1ca
semtetog <brennodmoraes@gmail.com>
2025-10-27 18:42:50 -0300

    chore: Remover arquivo de teste test_ajax.php

--- admin/actions/test_ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Teste AJAX SIMPLES
-ob_start();
-ob_clean();
-header('Content-Type: application/json');
-echo json_encode(['success' => true, 'message' => 'Teste funcionou!', 'data' => $_POST]);
-ob_end_flush();
-exit;
-
