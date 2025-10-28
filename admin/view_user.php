05fab2fbea4f9a47c678d81973125ea59725d6bf
semtetog <brennodmoraes@gmail.com>
2025-10-27 23:26:13 -0300

    feat: Redesenhar subcards com Ã­cones menores Ã  esquerda dos tÃ­tulos

--- admin/view_user.php
+++ admin/view_user.php
@@ -563,29 +563,38 @@ require_once __DIR__ . '/includes/header.php';
             <span class="meta-label">Kcal / dia</span>
         </div>
         <div class="meta-card-macros">
-            <div>
-                <span class="editable-value" 
-                      data-field="carbs_g" 
-                      data-user-id="<?php echo $user_id; ?>"
-                      data-original="<?php echo $macros_goal['carbs_g']; ?>"
-                      title="Clique para editar"><?php echo $macros_goal['carbs_g']; ?>g</span>
-                Carboidratos
+            <div class="macro-item">
+                <i class="fas fa-bread-slice icon"></i>
+                <div class="macro-content">
+                    <span class="editable-value" 
+                          data-field="carbs_g" 
+                          data-user-id="<?php echo $user_id; ?>"
+                          data-original="<?php echo $macros_goal['carbs_g']; ?>"
+                          title="Clique para editar"><?php echo $macros_goal['carbs_g']; ?>g</span>
+                    <label>Carboidratos</label>
+                </div>
             </div>
-            <div>
-                <span class="editable-value" 
-                      data-field="protein_g" 
-                      data-user-id="<?php echo $user_id; ?>"
-                      data-original="<?php echo $macros_goal['protein_g']; ?>"
-                      title="Clique para editar"><?php echo $macros_goal['protein_g']; ?>g</span>
-                Proteínas
+            <div class="macro-item">
+                <i class="fas fa-drumstick-bite icon"></i>
+                <div class="macro-content">
+                    <span class="editable-value" 
+                          data-field="protein_g" 
+                          data-user-id="<?php echo $user_id; ?>"
+                          data-original="<?php echo $macros_goal['protein_g']; ?>"
+                          title="Clique para editar"><?php echo $macros_goal['protein_g']; ?>g</span>
+                    <label>Proteínas</label>
+                </div>
             </div>
-            <div>
-                <span class="editable-value" 
-                      data-field="fat_g" 
-                      data-user-id="<?php echo $user_id; ?>"
-                      data-original="<?php echo $macros_goal['fat_g']; ?>"
-                      title="Clique para editar"><?php echo $macros_goal['fat_g']; ?>g</span>
-                Gorduras
+            <div class="macro-item">
+                <i class="fas fa-bacon icon"></i>
+                <div class="macro-content">
+                    <span class="editable-value" 
+                          data-field="fat_g" 
+                          data-user-id="<?php echo $user_id; ?>"
+                          data-original="<?php echo $macros_goal['fat_g']; ?>"
+                          title="Clique para editar"><?php echo $macros_goal['fat_g']; ?>g</span>
+                    <label>Gorduras</label>
+                </div>
             </div>
         </div>
     </div>
