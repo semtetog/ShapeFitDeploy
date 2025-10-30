0bab1ff9f3e793ed794cd09845e0b62b936e1914
semtetog <brennodmoraes@gmail.com>
2025-10-30 18:15:45 -0300

    debug: Habilitar display_errors temporariamente para debug do erro 500

--- admin/actions/load_routine_days.php
+++ admin/actions/load_routine_days.php
@@ -1,6 +1,11 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
+// Habilitar exibição de erros temporariamente para debug
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../../includes/config.php';
 require_once __DIR__ . '/../includes/functions_admin.php';
 $conn = require __DIR__ . '/../../includes/db.php';
